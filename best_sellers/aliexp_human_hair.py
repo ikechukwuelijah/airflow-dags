b0def8fcee795803776029b0ad33f21656520749
Ikechukwu Elijah <98159873+ikechukwuelijah@users.noreply.github.com>
2025-08-11 21:52:28 +0100

    Update aliexp_human_hair.py

--- best_sellers/aliexp_human_hair.py
+++ best_sellers/aliexp_human_hair.py
@@ -7,7 +7,7 @@ url = "https://aliexpress-business-api.p.rapidapi.com/affiliate-hot-products.php
 querystring = {"keyWord":"Human Hair","currency":"USD","lang":"en","country":"NG"}
 
 headers = {
-	"x-rapidapi-key": "f38eae887bmsh5211e33c97c1c50p125cafjsnec52eb060a05",
+	"x-rapidapi-key": "",
 	"x-rapidapi-host": "aliexpress-business-api.p.rapidapi.com"
 }
 
